bb594660fd74cc052418bbaa9c8819c7389c363c
dreamclinger <chenhaomails@gmail.com>
2015-08-03 22:11:37 +0800

    try alter recov by cvx/cosamp, fail; try 1 side recov (Rz*inv(D'A') = ADS), fail

--- sparse_cyclic_spectrum/test.m
+++ sparse_cyclic_spectrum/test.m
@@ -23,12 +23,12 @@ end
 
 sig.x = fsk_real(1:256);
 sig.N = length(sig.x);
-% test1, xcorr -> cyclic_spec -> cs_cyc_spec
-
-%[Spec_cs, f, a] = cyclic_xcorr(sig.x, sig.N, sig.fs, 'show'); 
+%sig.x=(randn(1,sig.N)+sqrt(-1)*randn(1,sig.N))./(sqrt(2)); % complex noise
 
 % test2, cs_cyc_spec + feature extract
 
-[Spec_cs, f, a] = cs_cyclic_spectrum(sig.x, sig.N, sig.fs, 'show'); 
-[out] = feature_test(Spec_cs, f, [-0.05 +0.05], a, [+0.01 +0.15]);
-norm(out)
+[Spec_cs, Spec, f, a] = cs_cyclic_spectrum(sig.x, sig.N, sig.fs, 'show'); 
+[out1] = feature_test(Spec, f, [-0.2 +0.2], a, [-0.2 +0.2]);
+[out2] = feature_test(Spec_cs, f, [-0.2 +0.2], a, [-0.2 +0.2]);
+norm(out1)
+norm(out2)
