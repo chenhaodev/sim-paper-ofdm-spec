91163a99347d4f087c548e6e46f3afd24f98711a
dreamclinger <chenhaomails@gmail.com>
2015-08-10 23:10:46 +0800

    update Pn, Qm, B. Pn, Qm is failed

--- sparse_cyclic_spectrum/test05_vec_sr_sx.m
+++ sparse_cyclic_spectrum/test05_vec_sr_sx.m
@@ -42,22 +42,18 @@ i = 1;
 x = x';
 
 % Equivalent cyclic spectrum
-% Rx = x*x' <=> S in cyclic_spectrum.m 
 Dft = dctmtx(N);%dftmtx(N);
 %D = fftshift(Dft); % choose DFT matrix
 D = Dft;
-Rx_o = x*x';
-%Sx = D*Rx*D;
-
+Rx = x*x';
 
 %the following canbe optimised !!!
 
 %R generation, ref[1].eq(7)
-Rx = zeros(N,N);
+R = zeros(N,N);
 for nn = 1:N
 	for v = 1:(N-nn+1) % last nn-1 items in each row is zero.
-		%Rx(nn,v) = x(nn)* x(mod(nn+v-1, N));
-        Rx(nn,v) = x(nn)* x(nn+v-1);
+        R(nn,v) = x(nn)* x(nn+v-1);
 	end
 end
 
@@ -71,36 +67,18 @@ for nn = 1:N
 	end
 end
 
-%Pn, Qm generate, ref[1].eq(43,44)
-Pn = zeros(N*N, N*(N+1)/2);
-Qn = zeros(N*N, N*(N+1)/2); %what's kron delta?
-for v = 1:N
-	for nn = 1:(N-v+1)  %eq(43)
-		p = (v-1)*N - (v-1)*(v-2)/2 + (nn-1) + 1;
-		q1 = (v-1+nn-1)*N + (nn-1) + 1;  %eq(42)
-		q2 = (nn-1)*N + (nn-1) + (v-1) + 1;  
-		Pn(q1, p) = 1; Pn(q2, p) = 1;
-		if (v == 1)
-			kron_delta = 1;
-		else
-			kron_delta = 0;
-		end 
-		Qn(p,q1) = 1; Qn(p,q2) = 1/2 + (1/2)*kron_delta;
-	end
-end
-
 %rx generate, ref[1].eq(8)
 rx = [];
 for nn = 1:N
 	for v = 1:(N-nn+1) 
-		tmp = Rx(nn,v);
+		tmp = R(nn,v);
 		rx = [rx, tmp];
 	end
 end
 
 % assert: 
 t0a = B*rx'; 
-t0b = reshape(Rx, 1, N*N);
+t0b = reshape(R, 1, N*N);
 if ( (norm(t0a - t0b') / length(t0a)) < 0.003)
     disp(' vec{R} = B*rx (?) ... yes');
 else
@@ -108,8 +86,6 @@ else
 end
 
 %generate cyclic spectrum 
-%ver1: Sx = D*Rx*D;
-%ver2:
 Sx_tmp = zeros(N,N);
 for v = 1:N 
 	Gv = zeros(N,N);
@@ -120,7 +96,7 @@ for v = 1:N
 		end
 	end
 	Dv(v,v) = 1;  % make groups of diag matrix that all entries are '0' but (v,v) = 1
-	Sx_tmp = Sx_tmp + Gv*Rx*Dv; % ref[1].eq(9)
+	Sx_tmp = Sx_tmp + Gv*R*Dv; % ref[1].eq(9)
 	% save Gv, Dv
 	Gv_save(:,:,v) = Gv;
 	Dv_save(:,:,v) = Dv;
@@ -130,11 +106,8 @@ Sx = Sx_tmp*D; %ref[1].eq(10), but eq(12) says vec{Sx * F^-1}... ?
 % Vectorize xcorr and cyclic spectrum
 matrix_load = 'no';
 Sx_r = reshape(Sx, 1, N*N); %reshape the cyclic spectrum
-%rx = reshape(Rx, 1, N*N); %reshape the xcorr
-%B = eye(N^2);
-%H = kron((eye(N))',D)*B;
 H_tmp = zeros(N^2,N^2);
-for v = 1:N % calculate the matrix that map Rx => Sx_tmp_sum
+for v = 1:N % calculate the matrix that map R => Sx_tmp_sum
 	Gv = Gv_save(:,:,v);
 	Dv = Dv_save(:,:,v);
 	H_tmp = H_tmp + kron(Dv', Gv);  
@@ -165,38 +138,90 @@ cs.ratio = 4;
 cs.iter = 32;
 cs.N = N;
 cs.M = round(cs.N/cs.ratio); % num of sensing points
+M = cs.M;
 Phi = pn_gen(cs.M,cs.N);
 y = Phi*x;
+Rz = y*y';
 
+%{
 %R generation via CS sampls, ref[1].eq(14)
-Cy = zeros(cs.M,cs.M);
+Rz = zeros(cs.M,cs.M);
 for nn = 1:cs.M
 	for v = 1:(cs.M-nn+1) % last nn-1 items in each row is zero.
-		%Cy(nn,v) = x(nn)* x(mod(nn+v-1, cs.M));
-        Cy(nn,v) = y(nn)* y(nn+v-1);
+        Rz(nn,v) = y(nn)* y(nn+v-1);
+	end
+end
+%}
+
+%vec{Rz}
+rz = [];
+for nn = 1:M
+	for v = 1:(M-nn+1) 
+		tmp = Rz(nn,v);
+		rz = [rz, tmp];
 	end
 end
 
-Cy_r = reshape(Cy, 1, cs.M*cs.M);
-Qm = eye(cs.M*cs.M); Pn = eye(N*N);
+%Pn, Qm generate, ref[1].eq(43,44)
+Pn = zeros(N*N, N*(N+1)/2);
+for v = 1:N
+	for nn = 1:(N-v+1)  %eq(43)
+		p = (v-1)*N - (v-1)*(v-2)/2 + (nn-1) + 1;
+		q1 = (v-1+nn-1)*N + (nn-1) + 1;  %eq(42)
+		q2 = (nn-1)*N + (nn-1) + (v-1) + 1;  
+		Pn(q1, p) = 1; Pn(q2, p) = 1;
+	end
+end
+Qm = zeros(M*(M+1)/2, M*M);
+for v = 1:M
+	for nn = 1:(M-v+1)
+		p = (v-1)*M - (v-1)*(v-2)/2 + (nn-1) + 1;
+		q1 = (v-1+nn-1)*M + (nn-1) + 1; 
+		q2 = (nn-1)*M + (nn-1) + (v-1) + 1;  
+		if (v == 1)  % gen kron delta
+			kron_delta = 1;
+		else
+			kron_delta = 0;
+		end 
+		Qm(p,q1) = 1; Qm(p,q2) = 1/2 + (1/2)*kron_delta; %eq(44)
+	end
+end
+
+% assert: 
+Rx = x*x';
+t0c = Pn*rx'; 
+t0d = reshape(Rx, 1, N*N); 
+t0d = t0d';
+if ( (norm(t0c - t0d) / length(t0c)) < 0.003)
+    disp(' vec{Rx} = Pn*rx (?) ... yes');
+else
+    error('  vec{Rx} = Pn*rx (?) ... no');
+end
+t0e = rz'; 
+t0f = reshape(Rz, 1, M*M);
+t0f = Qm*t0f';
+if ( (norm(t0e - t0f) / length(t0c)) < 0.003)
+    disp(' rz = Qm*vec{Rz} (?) ... yes');
+else
+    error('  rz = Qm*vec{Rz} (?) ... no');
+end
+
 A = Qm*kron(Phi,Phi)*Pn*H_inv*W_r; % equivalent sensing matrix for CS, ref[1].eq(17)
-b = Cy_r';
+b = rz';
 
 % assert: 
 t3a = A*Sx_r';
 t3b = b;
 if ( (norm(t3a - t3b) / length(t3a)) < 0.005)
-    disp('test: Cy_r = A*Sx_r (?) ... yes');
+    disp('test: Rz_r = A*Sx_r (?) ... yes');
 else
-    error('test: Cy_r = A*Sx_r (?) ... no');
+    error('test: Rz_r = A*Sx_r (?) ... no');
 end
 
 % Link compressed covariance and vectorized cyclic spectrum 
-% 1 Phi*x*x'*Phi' = Phi*Rx*Phi' =y*y' = Cy; ..ok
-% 2. H*rx' = W_r*Sx_r'; ..ok
-% 3. Cy_r' = kron(Phi,Phi)*rx'; .. ok
-% so Cy_r' = kron(Phi,Phi)*H_inv*W_r*rx', where rx is sparse;
-% Problem: sparse hat_m is not concentrated!
+% H*rx' = W_r*Sx_r'; ..ok
+% Rz_r' = kron(Phi,Phi)*rx'; .. ok
+% Rz_r' = kron(Phi,Phi)*H_inv*W_r*rx', where rx is sparse;
 
 cvx_begin
     variable hatX(N^2);
