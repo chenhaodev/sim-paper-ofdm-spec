29d32f42b64dbf3089f5be2bafc8c2b84fa0b1ac
dreamclinger <chenhaomails@gmail.com>
2015-08-13 10:25:25 +0800

    suc recov, pattern disp; but not very diff from noise recov, need further process for GLRT and CS-cov-thres

--- sparse_cyclic_spectrum/Test/test06_vec_sr_sx.m
+++ sparse_cyclic_spectrum/Test/test06_vec_sr_sx.m
@@ -6,19 +6,21 @@ addpath('./Data/')
 
 % Header 
 
-sig.type = 'fsk'; % 'fsk'
+sig.type = 'bpsk'; % 'fsk'
 sig.fs = 1;
 sig.M = 1;
 
 if strcmpi(sig.type,'fsk') % default signal
-	load fsk.mat             
+	load fsk.mat
+    sig.x = fsk_real(1:64);
+elseif strcmpi(sig.type,'bpsk') % default signal
+	load bpsk.mat
+    sig.x = bpsk_w(1:64);
 else
 	error('signal type not exist!!');
 end
 
-sig.x = fsk_real(1:64);
-%sig.x = fsk_real(65:128);
-%sig.x= fsk_real(1:64)+(10*randn(1,64)); 
+sig.x= randn(1,64); %!!!!!!
 
 sig.x = sig.x ./ norm(sig.x);
 sig.N = length(sig.x);
@@ -116,6 +118,16 @@ for v = 1:N % calculate the matrix that map R => Rxc_sum
 end
 H = H_tmp* B; 
 W_r = kron((inv(D)).', eye(N));
+H_inv = ((H'*H)^(-1))*H'; %H*
+
+% assert: 
+t1a = W_r*Sx_r.'; 
+t1b = H*rx.';
+if ( (norm(t1a - t1b) / length(t1a)) < 1e-10)
+    disp('test: H*rx == W_r*Sx_r (?) ... yes');
+else
+    error('test: H*rx == W_r*Sx_r (?) ... no');
+end
 
 % Compressed sampling the signal
 cs.sparse = 16;
@@ -124,7 +136,7 @@ cs.iter = 32;
 cs.N = N;
 cs.M = round(cs.N/cs.ratio); % num of sensing points
 M = cs.M;
-Phi = pn_gen(cs.M,cs.N);
+Phi = randn(cs.M, cs.N);
 y = Phi*x;
 Rz = y*y.';
 
@@ -163,7 +175,6 @@ for v = 1:M
 	end
 end
 
-%{
 % assert: 
 Rx = x*x.';
 t2a = Pn*rx.'; 
@@ -180,22 +191,11 @@ if ( (norm(t2c - t2d) / length(t2c)) < 1e-10)
 else
     error('  rz = Qm*vec{Rz} (?) ... no');
 end
-%}
-
 
 % Link compressed covariance and vectorized cyclic spectrum 
 % H*rx.' = W_r*Sx_r.'; ..ok
 % rz.' = Qm*kron(Phi,Phi)*Pn*rx.'; .. ok
 % Hence, equivalent sensing matrix for CS, ref[1].eq(17):
-
-% assert: 
-t1a = W_r*Sx_r.'; 
-t1b = H*rx.';
-if ( (norm(t1a - t1b) / length(t1a)) < 1e-10)
-    disp('test: H*rx == W_r*Sx_r (?) ... yes');
-else
-    error('test: H*rx == W_r*Sx_r (?) ... no');
-end
 
 % assert: 
 t3a = Qm*kron(Phi,Phi)*Pn*rx.';
@@ -205,10 +205,18 @@ if ( (norm(t3a - t3b) / length(t3a)) < 1e-10)
 else
     error('test: rz = Qm*kron(Phi,Phi)*Pn*rx (?) ... no');
 end
-
-A = Qm*kron(Phi,Phi)*Pn*pinv(H)*W_r; 
+A = Qm*kron(Phi,Phi)*Pn*H_inv*W_r;
 b = rz.';
 
+    cvx_begin
+        variable x(N^2);
+        minimize( norm(A*x-b)+5.4556*norm(x,1) );
+    cvx_end
+hat_m = (vec2mat(x, N, N)).';
+figure; mesh(abs(hat_m));
+
+
+%{
 cvx_begin
     variable hatX(N^2);
     minimize(norm(hatX,1));
@@ -216,7 +224,31 @@ cvx_begin
 cvx_end
 hat_m = (vec2mat(hatX, N, N)).';
 figure; mesh(abs(hat_m));
+%figure; mesh(abs(vec2mat(W_r*hatX, N,N)*D));
+%}
 
 % Extract feature energy from recov spectrum
 %[out] = feature_extract(abs(hat_m), 1:N, 0.2, 1:N, 0.2);
 %norm(out)
+
+%{
+gamma = logspace( -2, 2, 20 );
+l2norm = zeros(size(gamma));
+l1norm = zeros(size(gamma));
+fprintf( 1, '   gamma       norm(x,1)    norm(A*x-b)\n' );
+fprintf( 1, '---------------------------------------\n' );
+for k = 1:length(gamma),
+    fprintf( 1, '%8.4e', gamma(k) );
+    cvx_begin
+        variable x(N^2);
+        minimize( norm(A*x-b)+gamma(k)*norm(x,1) );
+    cvx_end
+    l1norm(k) = norm(x,1);
+    l2norm(k) = norm(A*x-b);
+    fprintf( 1, '   %8.4e   %8.4e\n', l1norm(k), l2norm(k) );
+end
+plot( l1norm, l2norm );
+xlabel( 'norm(x,1)' );
+ylabel( 'norm(A*x-b)' );
+grid on
+%}
